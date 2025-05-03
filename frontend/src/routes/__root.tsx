9b16fc4e6177f9c6a2cbe7f75b879d971c49c9ef
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-03 10:44:58 +0200

    I have started creating layout of main site

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,7 +4,10 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
+      <div className="p-2 flex gap-2 items-center">
+
+        <p className='bg-amber-400 px-4 py-1'>Złotówka</p>
+
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{' '}
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { FaArrowRight } from "react-icons/fa";
 
-export const Route = createFileRoute('/')({
-  component: Index,
-})
+export const Route = createFileRoute("/")({
+	component: Index,
+});
 
 function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<div className="flex flex-col gap-2 p-5 items-start">
+				<h3 className="text-4xl">Take care of your money with <span className="text-red-600">Złotówka</span></h3>
+				<p className="text-lg">App that can help you manage your spends & wages</p>
+
+        <Link to="/about" className="[&.active]:font-bold">
+        <button className="flex gap-2 items-center bg-red-100 py-2 px-5 rounded-lg text-lg cursor-pointer">Get started <FaArrowRight className="text-red-500" />
+        
+        </button>
+        </Link>
+
+			</div>
+		</div>
+	);
 }
