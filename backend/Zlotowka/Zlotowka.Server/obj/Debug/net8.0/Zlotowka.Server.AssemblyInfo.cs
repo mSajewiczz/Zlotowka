ad71fe4e15c77eb6e2124d13afc32957dbeeb128
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-03 23:39:49 +0200

    Control commit #2

--- backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
+++ backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357b0a6ed35ef8dc23d5f6b8e7c2f1d41d4f215f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b98042fc5bea0c71ba17dc6c1b3ddaaf1582b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
