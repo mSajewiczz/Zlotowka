51dcd6116d3f45e39bf7c7e8623c70b9c6b33da5
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-04 08:58:13 +0200

    Modified few modules

--- backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
+++ backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d454ef44648fc4fed39757b43b3ae1aff1cddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70226f18eb97ebb7ae525d77040203c0cd1f1bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
