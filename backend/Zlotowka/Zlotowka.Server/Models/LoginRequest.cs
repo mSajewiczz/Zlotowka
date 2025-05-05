dbf97263443ee1320135a84035420bc7b771a1ed
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-05 09:31:29 +0200

    Modified controller

--- backend/Zlotowka/Zlotowka.Server/Models/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zlotowka.Server.Models;
-
-public class LoginRequest
-{
-    public string UserName { get; set; }
-    public string Password { get; set; }
-}
--- backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
+++ backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5041e242f182dad7c15cbeef8f8d7f6c5b01a1e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9503009bc9b81a967b31780c14503c1195b8ac75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
