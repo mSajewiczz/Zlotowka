9141cfd901812fac4bcdedc094b4c4f5b2e16345
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-06 13:17:02 +0200

    Modified some modules

--- frontend/src/components/Spends/Spends.tsx
+++ frontend/src/components/Spends/Spends.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import ManageForm from "../ManageForm/ManageForm";
 import { FaPlus } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
 export default function Spends() {
 	const [showForm, setShowForm] = useState(false);
 	const [data, setData] = useState([]);
-    const [count, setCount] = useState();
+    const [count, setCount] = useState(0);
 
 	const title = "Spend";
 
@@ -16,17 +17,19 @@ export default function Spends() {
 			},
 		});
 
+
+
 		if (response.ok) {
 			const data = await response.json();
 			setData(data);
 
-        
-
-		} else {
+        } else {
             console.log("sth went wrong");
             
 		}
 	}
+
+
 
 	const handleOpenSpendForm = () => {
 		setShowForm(true);
@@ -52,12 +55,12 @@ export default function Spends() {
 				<h3 className="pb-2 text-xl text-amber-700 bg-white py-1 px-2">List of your spends</h3>
 				<ul className="flex flex-col gap-2">
 					{data.length === 0 ? <p className="text-red-800">You have no spends, congratulations!</p> : data.map((spend: any) => (
-						<li key={spend.title}  className="bg-white text-blue-600 py-1 px-2">
-                        {spend.title} - <span className="text-red-500">{spend.amount} zł</span> - {spend.date}
+						<li key={spend.title}  className="bg-white text-blue-600 py-1 px-2 flex gap-2 items-center">
+                        {spend.title} - <span className="text-red-500">{spend.amount} zł</span> - {spend.date} <button className="cursor-pointer"><MdDelete /></button>
                    </li>
 					))}
 				</ul>
-                <p>SUM:{count} </p>
+                <p>SUM: {count} zł</p>
 			</div>
 
 			{showForm && (
