a7b8b97cf34c59d8b3dca99eed763edbfd9582df
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-07 08:57:40 +0200

    Resolve merge problem

--- backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
+++ backend/Zlotowka/Zlotowka.Server/obj/Debug/net8.0/Zlotowka.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91724a610e175ff892580aec479a67e59e1fff4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2448cfd38794b68776c057234b67a7671d5cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zlotowka.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
